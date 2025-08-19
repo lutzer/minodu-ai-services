ad6888b5cbd5c8eacb0d87057bd66923af7cc5e3
Lutz <lutz@lu-re.de>
2025-08-19 15:25:56 +0100

    removes rag api

--- rag-api/src/config.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-const config = {
-    venvPath: path.join(__dirname, "../../rag/.venv/bin/python"),
-    cliPath : path.join(__dirname, "../../rag/src/rag.py"),
-    language: "fr"
-};
-
-export default config;
